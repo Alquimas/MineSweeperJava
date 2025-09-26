72cd39a17d7842598ace66231d8be80a51b2a86c
luther5000 <luterogoulart@gmail.com>
2025-09-26 20:20:38 -0300

    feat(model): adição da criação do TabuleiroFront, da atualização de um quadrado e dos métodos que retornam informações sobre os quadrados do tabuleiro
    Foi adicionada a lógica de criação do TabuleiroFront, que será criado
    com QuadradosFront em seu estado default (nenhuma informação além da
    localização).
    
    Foi adicionada a lógica de atualização de um quadrado no tabuleiro. O
    método receberá um novo QuadradoFront e olhará a sua localização. Após
    isso irá substituir o quadrado existente naquela localização por este
    novo quadrado.
    
    Foram adicionados os métodos que retornam informações sobre algum
    quadrado do tabuleiro. Esses métodos recebem a localização do quadrado a
    ser buscado e retornam o resultado daquele método na própria classe do
    quadrado.

--- src/main/java/org/minesweeper/model/TabuleiroFront.java
+++ src/main/java/org/minesweeper/model/TabuleiroFront.java
@@ -8,6 +8,58 @@ public class TabuleiroFront {
     private int linha_size;
     private int coluna_size;
 
+    public TabuleiroFront(int linha_size, int coluna_size){
+        this.linha_size = linha_size;
+        this.coluna_size = coluna_size;
+
+        for (int i = 0; i < linha_size; i++){
+            tabuleiro = new ArrayList<>();
+            for (int j = 0; j < coluna_size; j++){
+                Localizacao localizacao = new Localizacao(i, j);
+                tabuleiro.get(i).add(criaQuadrado(localizacao));
+            }
+        }
+    }
+
+    private QuadradoFront criaQuadrado(Localizacao localizacao){
+        return new QuadradoFront(localizacao);
+    }
+
+    public void atualizaQuadrado(QuadradoFront quadrado){
+        int linha = quadrado.getLocalizacao().getLinha();
+        int coluna = quadrado.getLocalizacao().getColuna();
+
+        tabuleiro.get(linha).set(coluna, quadrado);
+    }
+    
+    public boolean isAberto(Localizacao localizacao) {
+        int linha = localizacao.getLinha();
+        int coluna = localizacao.getColuna();
+
+        return tabuleiro.get(linha).get(coluna).isAberto();
+    }
+
+    public boolean isMarcado(Localizacao localizacao) {
+        int linha = localizacao.getLinha();
+        int coluna = localizacao.getColuna();
+
+        return tabuleiro.get(linha).get(coluna).isMarcado();
+    }
+
+    public boolean isBomba(Localizacao localizacao){
+        int linha = localizacao.getLinha();
+        int coluna = localizacao.getColuna();
+
+        return tabuleiro.get(linha).get(coluna).isBomba();
+    }
+
+    public int getVizinhosPerigosos(Localizacao localizacao){
+        int linha = localizacao.getLinha();
+        int coluna = localizacao.getColuna();
+
+        return tabuleiro.get(linha).get(coluna).getNumero();
+    }
+
     public int getLinha_size() {
         return linha_size;
     }
