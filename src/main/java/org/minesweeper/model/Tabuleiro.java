10b5b3b7798220d526370045e0d4ad31628cb070
luther5000 <luterogoulart@gmail.com>
2025-09-26 20:30:15 -0300

    feat(model): adição da criação de um novo tabuleiro para o jogo
    Foi adicionada a lógica para criar um tabuleiro novo. São criados todos
    os quadrados do jogo, os com e o sem bomba (seguindo o valor da entrada
    do método), e depois eles são adicionados de forma aleatória ao
    tabuleiro.

--- src/main/java/org/minesweeper/model/Tabuleiro.java
+++ src/main/java/org/minesweeper/model/Tabuleiro.java
@@ -3,6 +3,7 @@ package org.minesweeper.model;
 import org.minesweeper.service.AcaoTabuleiro;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Tabuleiro implements TabuleiroInterface {
     private ArrayList<ArrayList<Quadrado>> tabuleiro;
@@ -19,6 +20,47 @@ public class Tabuleiro implements TabuleiroInterface {
     @Override
     public <R> R accept(AcaoTabuleiro acaoTabuleiro) {
         return acaoTabuleiro.visitTabuleiro();
+    }
+
+    /**
+     * Cria um novo tabuleiro para o jogo.
+     *
+     * @param linha A quantidade de linhas do tabuleiro.
+     * @param coluna A quantidade de colunas do tabuleiro.
+     * @param bombas A quantidade de bombas do tabuleiro.
+     */
+    public void criaTabuleiro(int linha, int coluna, int bombas){
+        int numQuadrados = linha * coluna;
+        Random r = new Random();
+        ArrayList<Quadrado> quadrados = new ArrayList<>();
+
+        // Cria quadrados sem bomba
+        for (int i = 0; i < numQuadrados - bombas; ++i){
+            quadrados.add(new Quadrado(false, false, false));
+        }
+
+        // Cria quadrados com bomba
+        for (int i = 0; i < bombas; ++i){
+            quadrados.add(new Quadrado(true, false, false));
+        }
+
+        // Conta quantos quadrados foram adicionados
+        int cont = 0;
+
+        // Adiciona os quadrados de forma aletória no tabuleiro
+        for (int i = 0; i < linha; ++i){
+            tabuleiro.add(new ArrayList<>());
+
+            for (int j = 0; j < coluna; ++j){
+                // Reduz o limite de geração para ficar de acordo com o novo
+                // tamanho do array após a remoção do elemento sorteado
+                int num = r.nextInt(numQuadrados - cont++);
+                Quadrado atual = quadrados.remove(num);
+
+                atual.setLocalizacao(new Localizacao(i, j));
+                tabuleiro.get(i).add(atual);
+            }
+        }
     }
 
     public int getLinha_size() {
