6f66c323a1cce844dc0325e924f743948d649937
luther5000 <luterogoulart@gmail.com>
2025-09-27 18:08:40 -0300

    feat(service): criação da classe abstrata que atua como interface das classes visitoras do padrão Visitor

---
+++ src/main/java/org/minesweeper/service/AcaoTabuleiro.java
@@ -0,0 +1,9 @@
+package org.minesweeper.service;
+
+import org.minesweeper.model.*;
+
+public abstract class AcaoTabuleiro {
+    Localizacao localizacao;
+
+    public abstract <R> R visitTabuleiro();
+}
