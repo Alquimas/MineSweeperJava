6a7f513d57c90bb7841cb9d95ca6be68cb315804
luther5000 <luterogoulart@gmail.com>
2025-09-29 17:20:38 -0300

    fix(service, model): alterações para conformar código com os comentários realizados no pull request

--- src/main/java/org/minesweeper/model/Localizacao.java
+++ src/main/java/org/minesweeper/model/Localizacao.java
@@ -15,16 +15,8 @@ public class Localizacao {
         return linha;
     }
 
-    public void setLinha(int linha) {
-        this.linha = linha;
-    }
-
     public int getColuna() {
         return coluna;
-    }
-
-    public void setColuna(int coluna) {
-        this.coluna = coluna;
     }
 
     @Override
