39882af76ab0dd91dbcaf99a08c940f289c94474
luther5000 <luterogoulart@gmail.com>
2025-10-04 13:36:07 -0300

    feat(navigator): adição da estutura de NavegadorTelaFimJogo junto da sua interface que a view utiliza para se comunicar com ele

---
+++ src/main/java/org/minesweeper/navigator/NavegadorTelaFimJogo.java
@@ -0,0 +1,26 @@
+package org.minesweeper.navigator;
+
+import org.minesweeper.coordinator.CoordenadorListener;
+import org.minesweeper.view.ViewTelaFimJogo;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class NavegadorTelaFimJogo implements NavegadorTelaFimJogoListener {
+    private ViewTelaFimJogo view;
+    private ArrayList<CoordenadorListener> listeners;
+    private JFrame tela;
+
+    @Override
+    public void finalizarJogo() {}
+
+    @Override
+    public void reiniciarJogo() {}
+
+    public void iniciar(JFrame tela, boolean ganhou){}
+    private void destruir(){}
+    private void ganhouJogo(){}
+    private void perdeuJogo(){}
+    public void subscribe(CoordenadorListener coordenadorListener){}
+    public void unsubscribe(CoordenadorListener coordenadorListener){}
+}
