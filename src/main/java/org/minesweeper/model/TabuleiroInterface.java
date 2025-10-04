c8f55f39d9b71526d73d9a26a8184206cbb52b00
luther5000 <luterogoulart@gmail.com>
2025-10-03 21:23:43 -0300

    feat(model): adição da funcionalidade de verificar se o jogador ganhou o jogo

--- src/main/java/org/minesweeper/model/TabuleiroInterface.java
+++ src/main/java/org/minesweeper/model/TabuleiroInterface.java
@@ -4,4 +4,5 @@ import org.minesweeper.service.AcaoTabuleiro;
 
 public interface TabuleiroInterface {
     public abstract <R> R accept(AcaoTabuleiro acaoTabuleiro);
+    public abstract boolean ganhou();
 }
